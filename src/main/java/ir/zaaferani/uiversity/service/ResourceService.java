c07a700ded8178068cccd61cb8618447341eafb9
Hassan <zaaferani.arta@gmail.com>
2017-02-04 08:06:16 +0330

    add update and delete

--- src/main/java/ir/zaaferani/uiversity/service/ResourceService.java
+++ src/main/java/ir/zaaferani/uiversity/service/ResourceService.java
@@ -4,12 +4,15 @@ package ir.zaaferani.uiversity.service;
 import ir.zaaferani.uiversity.models.ElementModel;
 import ir.zaaferani.uiversity.models.ServiceResult;
 
+import java.util.UUID;
+
 public interface ResourceService {
 
     ServiceResult addRecord(ElementModel elementRecord);
-//    ServiceResult updateRecord(ElementModel elementRecord);
-//    ServiceResult deleteRecord(ElementModel elementRecord);
+    ServiceResult updateRecord(ElementModel elementRecord);
+    ServiceResult deleteRecord(ElementModel elementRecord);
 //    ServiceResult getRecord(ElementModel elementRecord);
 //    ServiceResult getParents(ResourceModel resourceModel);
 //    ServiceResult getChild(ElementModel elementModel);
+    ServiceResult getResource(UUID serviceId, UUID elementId);
 }
--- src/main/java/ir/zaaferani/uiversity/service/ResourceServiceImpl.java
+++ src/main/java/ir/zaaferani/uiversity/service/ResourceServiceImpl.java
@@ -3,10 +3,13 @@ package ir.zaaferani.uiversity.service;
 import ir.zaaferani.uiversity.models.ElementModel;
 import ir.zaaferani.uiversity.models.ServiceResult;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ResourceServiceImpl implements ResourceService {
@@ -18,16 +21,37 @@ public class ResourceServiceImpl implements ResourceService {
 	}
 
 	@Override
-	public ServiceResult addRecord(ElementModel elementModel) {
-		ServiceResult res;
+	public ServiceResult addRecord(ElementModel elementRecord) {
+		return doRequest(urls.get("url.resource_controller.add"), elementRecord);
+	}
 
-		RestTemplate restTemplate = new RestTemplate();
-		try{
-			res = restTemplate.postForObject(urls.get("url.resource_controller.add"), elementModel, ServiceResult.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-			res = new ServiceResult(new ArrayList<String>());
+	@Override
+	public ServiceResult updateRecord(ElementModel elementRecord) {
+		return doRequest(urls.get("url.resource_controller.update"), elementRecord);
+	}
+
+	@Override
+	public ServiceResult deleteRecord(ElementModel elementRecord) {
+		return doRequest(urls.get("url.resource_controller.delete"), elementRecord);
+	}
+
+	public ServiceResult getResource(UUID serviceId, UUID elementId) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("serviceId", serviceId);
+		if (elementId != null){
+			params.put("elementId", elementId);
 		}
-		return res;
+		return doRequest(urls.get("url.resource_controller.child"), params);
+	}
+
+	private ServiceResult doRequest(String url, Object params){
+		ServiceResult result;
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			result = restTemplate.postForObject(url, params, ServiceResult.class);
+		}catch (RestClientException e) {
+			result = new ServiceResult(new ArrayList<String>());
+		}
+		return result;
 	}
 }
